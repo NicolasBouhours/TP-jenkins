f99f1cee126be9c3b637bb02e55558c16f055ad6
formateur <formateur@cfcci53.fr>
2017-04-14 16:24:13 +0200

    Solution test

--- JenkinsTP/Program.cs
+++ JenkinsTP/Program.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace JenkinsTP
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
+            Console.WriteLine(Add(8, 6));
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Div(int a, int b)
+        {
+            return a/b;
         }
     }
 }
---
+++ TestUnitaire/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using JenkinsTP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestUnitaire
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            int result = Program.Add(5, 2);
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void TestDiv()
+        {
+            int result = Program.Div(12, 2);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException),
+    "Div by 0")]
+        public void TestDivFail()
+        {
+            int result = Program.Div(12, 0);
+            Assert.Fail();
+        }
+    }
+}
