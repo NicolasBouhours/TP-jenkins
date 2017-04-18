069ec6a842ea63d2d24f330a1419466516b7b7ff
formateur <formateur@cfcci53.fr>
2017-04-18 10:03:23 +0200

    Add nunit test

---
+++ UnitTestNUnit/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using JenkinsTP;
+
+namespace UnitTestNUnit
+{
+    class NUnitTest
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            int result = Program.Add(5, 2);
+            Assert.AreEqual(7, result);
+        }
+
+        [Test]
+        public void TestDiv()
+        {
+            int result = Program.Div(12, 2);
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void TestDivFail()
+        {
+            Assert.Throws<DivideByZeroException>(() => JenkinsTP.Program.Div(6, 0));
+        }
+    }
+}
