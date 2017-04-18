9a071d2bfb13275480393ba0092c6117659d9670
formateur <formateur@cfcci53.fr>
2017-04-18 14:18:42 +0200

    Push changes

--- JenkinsTP/Program.cs
+++ JenkinsTP/Program.cs
@@ -12,10 +12,6 @@ namespace JenkinsTP
         static void Main()
         {
             Console.WriteLine(Add(8, 6));
-            Console.WriteLine(Add(8, 6));
-            Console.WriteLine(Add(8, 6));
-            Console.WriteLine(Add(8, 6));
-            Console.WriteLine("azertyuiopqsdfghhjhkklwxxcvbzeertyusdfghdfrfguinjhigiefgfgfnjinjfgnjdfjbkjnfgjbjbfgjbgfbjkdfjkgfhguieruierueriuiuirtuirtbhvebuhvfbhuvfbh");
         }
 
         public static int Add(int a, int b)
@@ -30,12 +26,7 @@ namespace JenkinsTP
 
         public static int Adding(int a, int b)
         {
-            Console.WriteLine(Add(8, 6));
-            Console.WriteLine(Add(8, 6));
-            Console.WriteLine(Add(8, 6));
-            Console.WriteLine(Add(8, 6));
             Console.WriteLine("azertyuiopqsdfghhjhkklwxxcvbzeertyusdfghdfrfguinjhigiefgfgfnjinjfgnjdfjbkjnfgjbjbfgjbgfbjkdfjkgfhguieruierueriuiuirtuirtbhvebuhvfbhuvfbh");
-
             return a + b;
         }
     }
